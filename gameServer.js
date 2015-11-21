f8314f0a64f337486d9665287a5be8038d96d220
epadz <e.padzensky@comcast.net>
2015-11-21 13:51:30 -0600

    more groundwork

--- gameServer.js
+++ gameServer.js
@@ -8,13 +8,26 @@ var http = require("http"),
 var cats = ["A boy’s name", "A river", "An animal", "Things that are cold", "Insects", "TV Shows", "Things that grow", "Fruits", "Things that are black", "School subjects", "Movie titles", "Musical Instruments", "Authors", "Bodies of water", "A bird", "Countries", "Cartoon characters", "Holidays", "Things that are square", "In the NWT (Northwest Territories, Canada)", "Clothing", "A relative", "Games", "Sports Stars", "School supplies", "Things that are hot", "Heroes", "A girl’s name", "Fears", "TV Stars", "Colors", "A fish", "Fruits", "Provinces or States", "Sports equipment", "Tools", "Breakfast foods", "Gifts", "Flowers", "Ice cream flavors", "A drink", "Toys", "Cities", "Things in the kitchen", "Ocean things", "Nicknames", "Hobbies", "Parts of the body", "Sandwiches", "Items in a catalog", "World leaders/Poloticians", "School subjects", "Excuses for being late", "Ice cream flavors", "Things that jump/bounce", "Television stars", "Things in a park", "Foriegn cities", "Stones/Gems", "Musical instruments", "Nicknames", "Things in the sky", "Pizza toppings", "Colleges/Universities", "Fish", "Countries", "Things that have spots", "Historical Figures", "Something you’re afraid oF", "Terms of endearment", "Items in this room", "Drugs that are abused", "Fictional characters", "Menu items", "Magazines", "Capitals", "Kinds of candy", "Items you save up to buy", "Footware", "Something you keep hidden", "Items in a suitcase", "Things with tails", "Sports equiptment", "Crimes", "Things that are sticky", "Awards/ceremonies", "Cars", "Spices/Herbs", "Bad habits", "Cosmetics/Toiletries", "Celebrities", "Cooking utensils", "Reptiles/Amphibians", "Parks", "Leisure activities", "Things you’re allergic to", "Restaurants", "Notorious people", "Fruits", "Things in a medicine cabinet", "Toys", "Household chores", "Bodies of water", "Authors", "Halloween costumes", "Weapons", "Things that are round", "Words associated with exercise", "Sports", "Song titles", "Parts of the body", "Ethnic foods", "Things you shout", "Birds", "A girl’s name", "Ways to get from here to there", "Items in a kitchen", "Villains", "Flowers", "Things you replace", "Baby foods", "Famous duos and trios", "Things found in a desk", "Vacation spots", "Diseases", "Words associated with money", "Items in a vending machine", "Movie Titles", "Games", "Things you wear", "Beers", "Things at a circus", "Vegetables", "States", "Things you throw away", "Occupations", "Appliances", "Cartoon characters", "Types of drinks", "Musical groups", "Store names", "Things at a football game", "Trees", "Personality traits", "Video games", "Electronic gadgets", "Board games", "Things that use a remote", "Card games", "Internet lingo", "Offensive words", "Wireless things", "Computer parts", "Software", "Websites", "Game terms", "Things in a grocery store", "Reasons to quit your job", "Things that have stripes", "Tourist attractions", "Diet foods", "Things found in a hospital", "Food/Drink that is green", "Weekend Activities", "Acronyms", "Seafood", "Christmas songs", "Words ending in “-n”", "Words with double letters", "Children’s books", "Things found at a bar", "Sports played outdoors", "Names used in songs", "Foods you eat raw", "Places in Europe", "Olympic events", "Things you see at the zoo", "Math terms", "Animals in books or movies", "Things to do at a party", "Kinds of soup", "Things found in New York", "Things you get tickets for", "Things you do at work", "Foreign words used in English", "Things you shouldn’t touch", "Spicy foods", "Things at a carnival", "Things you make", "Places to hangout", "Animal noises", "Computer programs", "Honeymoon spots", "Things you buy for kids", "Things that can kill you", "Reasons to take out a loan", "Words associated with winter", "Things to do on a date", "Historic events", "Things you store items in", "Things you do everyday", "Things you get in the mail", "Things you save up to buy", "Things you sit/on", "Reasons to make a phone call", "Types of weather", "Titles people can have", "Things that have buttons", "Items you take on a road trip", "Things that have wheels", "Reasons to call 911", "Things that make you smile", "Ways to kill time", "Things that can get you fired", "Hobbies", "Holiday Activities"];
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "W"]; 
 var players = [];
-
+var games = [];
 //GAME PARAMS
 var gameTimeLimit = 20 * 1000; //seconds * 1000 = miliseconds
 var reviewTimeLimit = 30 * 1000;
 var maxPlayers = 6;
 var minPlaters = 3;
 
+//indexes
+var gid = 0; //game id
+var pid = 0; //player id
+
+
+//picks a random letter from the list. returns it. There are 20
+function pickLetter(){
+	return letters[parseInt(Math.floor(Math.random() * 20))];
+}
+//picks a random cattegory from the list. returns it. There are 216
+function pickCat(){
+	return cats[parseInt(Math.floor(Math.random() * 216))];
+}
 
 //an object that represents the game. A player can create a game. when a player creates and/or joins a game, set the game they joined to socket.game.
 //xgid is the id of the new game
@@ -22,9 +35,9 @@ var minPlaters = 3;
 function game(xgid, xhost){
 	this.gid = xgid;//game ID
 	this.host = xhost; //the socket that created it
-	this.members = {};
-	this.members[xhost.pid + ""] = xhost;
-	this.curLetter = null;
+	this.members = {}; //the sockets in the game
+	this.members[xhost.pid + ""] = xhost; //add the host to that list
+	this.curLetter = null; 
 	this.curCat = null;
 	this.curSubmissions = null;
 	this.state = 0 //0 is waiting for enough members. 1 is round is going on. 2 is answer review period.
@@ -151,7 +164,19 @@ io.sockets.on("connection", function(socket){
 	
 	socket.username = "";
 	socket.score = 0;
+	socket.pid = pid;
+	pid++;
 	
-	//function to emit randomly chosen letter and cattegory to the sockets in a room
-	//
+	g = new game(gid, socket);
+	games[gid + ""] = g;
+	gid++;
+	
+	//function to send the list of current games and number of players in it
+	//call that function on connection
+	
+	//function to handle joining a game
+	
+	//function to handle creating a game
+	
+	
 });
